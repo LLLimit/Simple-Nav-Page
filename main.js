69397eb2052a57b039031f860fe9325955f01451
LLLimit <luyichun1214@gmail.com>
2026-09-13 23:13:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // ── 图标 & 背景 配置 ────────────────────────────────────────
 const FAVICON_PROVIDER = 'google';
-const PROXY = 'https://map.darkduck.com';
+const PROXY = 'https://nav.darkduck.com';
 
 function withProxy(originUrl) {
   if (!PROXY) return originUrl;
